719667e45af26538b3a07d992c2c039ff0d3745e
Miłosz Panas <miloszpanas@gmail.com>
2018-10-15 12:00:19 +0200

    Add links, modify header on viewport size change

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 // SET THE VIEWPORT HEIGHT
 
-// get the viewport height and we multiple it by 1% to get a value for a vh unit
-
 let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+// resize on viewport change
+window.addEventListener("resize", () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+});
 
 // NAVIGATION
 
